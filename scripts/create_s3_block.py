503f046399aa5bdb5aa22baea6599a655e783c08
Addington Graham <akaigraham@gmail.com>
2023-06-05 16:33:16 -0400

    aws creds

--- scripts/create_s3_block.py
+++ scripts/create_s3_block.py
@@ -1,2 +1,9 @@
 from time import sleep 
 from prefect_aws import S3Bucket, AwsCredentials
+
+# create credentials block
+def create_aws_creds_block():
+    my_aws_creds_obj = AwsCredentials(
+        aws_access_key_id='',
+        aws_secret_access_key=''
+    )
