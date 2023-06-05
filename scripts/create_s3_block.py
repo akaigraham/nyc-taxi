22b713a5c8daf94d44910e583c2d997b382bf452
Addington Graham <akaigraham@gmail.com>
2023-06-05 16:43:45 -0400

    updated scripts

--- scripts/create_s3_block.py
+++ scripts/create_s3_block.py
@@ -1,9 +1,27 @@
 from time import sleep 
 from prefect_aws import S3Bucket, AwsCredentials
+import os
 
 # create credentials block
 def create_aws_creds_block():
+    AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     my_aws_creds_obj = AwsCredentials(
-        aws_access_key_id='',
-        aws_secret_access_key=''
+        aws_access_key_id=AWS_ACCESS_KEY,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
     )
+    my_aws_creds_obj.save(name="my-aws-creds", overwrite=True)
+    
+# create s3 bucket block 
+def create_s3_bucket_block():
+    aws_creds = AwsCredentials.load('my-aws-creds')
+    my_s3_bucket_obj = S3Bucket(
+        bucket_name='already-created-bucket-name',
+        credentials=aws_creds
+    )
+    my_s3_bucket_obj.save(name="s3-bucket-esample", overwrite=True)
+    
+if __name__ == '__main__':
+    create_aws_creds_block()
+    sleep(5)
+    create_s3_bucket_block()
